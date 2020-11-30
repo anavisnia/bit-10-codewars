59987d63ed3f9ee7e5a955534125ba2849af4b4c
anavisnia <73888794+anavisnia@users.noreply.github.com>
2020-11-30 19:56:29 +0200

    numberToStr and evenOrOdd num

---
+++ js/evenOrOdd.js
@@ -0,0 +1,16 @@
+/*
+https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/javascript
+*/
+
+function even_or_odd(number) {
+    if (number % 2 == 0) {
+        return 'Even';
+    } else {
+        return 'Odd';
+    }
+}
+
+console.log(even_or_odd(2), "Even");
+console.log(even_or_odd(0), "Even");
+console.log(even_or_odd(7), "Odd");
+console.log(even_or_odd(1), "Odd");
---
+++ js/numberToString.js
@@ -0,0 +1,11 @@
+/*
+https://www.codewars.com/kata/5265326f5fda8eb1160004c8/solutions/javascript
+*/
+
+function numberToString(num) {
+    let number = num.toString();
+    return number;
+}
+
+ console.log(numberToString(67), '67');
+ console.log(typeof(numberToString(2)));
