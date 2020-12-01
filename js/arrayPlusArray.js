e093dd8b0f76724bf7bef57124e100e9fc860fc6
anavisnia <73888794+anavisnia@users.noreply.github.com>
2020-12-01 12:25:15 +0200

    arrayPlusArray

---
+++ js/arrayPlusArray.js
@@ -0,0 +1,20 @@
+function arrayPlusArray(arr1, arr2) {
+    // return arr1 + arr2;
+    let atsFromAr1 = 0;
+    for (let i=0; i < arr1.length; i++) {
+        atsFromAr1 += arr1[i];
+    }
+        console.log(atsFromAr1);
+    let atsFromAr2 = 0;
+    for (let i=0; i < arr2.length; i++) {
+        atsFromAr2+=arr2[i];
+    }
+    console.log(atsFromAr2);
+    return atsFromAr1 + atsFromAr2;
+}
+
+
+console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]), '->', 21);
+console.log(arrayPlusArray([-1, -2, -3], [-4, -5, -6]), '->', -21);
+console.log(arrayPlusArray([0, 0, 0], [4, 5, 6]), '->', 15);
+console.log(arrayPlusArray([100, 200, 300], [400, 500, 600]), '->', 2100);
