f907e55925b0fda4d9d6797d6c7998e4a3ee6679
anavisnia <73888794+anavisnia@users.noreply.github.com>
2020-12-01 21:11:27 +0200

    initial commit of squareSum

---
+++ js/square-n-sum.js
@@ -0,0 +1,10 @@
+/*
+https://www.codewars.com/kata/515e271a311df0350d00000f/train/javascript
+*/
+
+function squareSum(numbers){
+
+}
+
+console.log(squareSum([1,2]), 5);
+console.log(squareSum([0, 3, 4, 5]), 50);
