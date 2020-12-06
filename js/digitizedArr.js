d1aebc73cf9fdfd94460b6a015bc61cb153b35bf
anavisnia <73888794+anavisnia@users.noreply.github.com>
2020-12-06 22:08:10 +0200

    some intresting answers from cw (shorter ver)

--- js/digitizedArr.js
+++ js/digitizedArr.js
@@ -13,3 +13,17 @@ function digitize(n) {
   }
 
 console.log(digitize(35231),[1,3,2,5,3]);
+
+
+// interesting answers from CODEWARS
+/*
+
+function digitize(n) {
+    return Array.from(String(n), Number).reverse();
+  }
+
+function digitize(n) {
+  return String(n).split('').map(Number).reverse()
+}
+
+*/
